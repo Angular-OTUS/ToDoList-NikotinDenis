77e9c976e42053f99f9473224e8c0c7609698057
Denis <nikotin.566@mail.ru>
2025-09-10 18:22:50 +0500

    Выполнено ДЗ №3

--- homework-app/src/app/components/to-do-list-item/to-do-list-item.ts
+++ homework-app/src/app/components/to-do-list-item/to-do-list-item.ts
@@ -1,11 +1,14 @@
+/* eslint-disable @angular-eslint/no-output-on-prefix */
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ButtonComponent } from '../button-component/button-component';
+import { ListItem } from '../../interfaces/list-item';
 
 @Component({
   selector: 'app-to-do-list-item',
-  imports: [CommonModule],
+  imports: [CommonModule,ButtonComponent],
   templateUrl: './to-do-list-item.html',
-  styleUrl: './to-do-list-item.css'
+  styleUrl: './to-do-list-item.css',
 })
 export class ToDoListItem {
 
@@ -18,7 +21,3 @@ export class ToDoListItem {
 
 }
 
-export interface ListItem{
-  id: number;
-  text: string;
-} 
